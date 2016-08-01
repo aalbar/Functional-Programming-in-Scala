6c1fcd11dd3c6a00810550841fa68fd2ffc92693
micho <gatin13@yahoo.com>
2016-08-01 13:13:30 +0900

    Count sentence occurrences

--- week 06/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ week 06/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -34,10 +34,10 @@ object Anagrams {
    *
    *  Note: you must use `groupBy` to implement this method!
    */
-  def wordOccurrences(w: Word): Occurrences = w.toLowerCase().groupBy(c => c).map(c => (c._1, c._2.size)).toList.sorted
+  def wordOccurrences(w: Word): Occurrences = w.toLowerCase().groupBy(c => c).map(c => (c._1, c._2.size)).toList sorted
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s.foldLeft("")(_ + _))
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
